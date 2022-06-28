363acae5920d7c2714f1919f01e490321d6c1add
Gaurav <gaurav.r@arbyte.com.np>
2022-06-29 01:44:14 +0545

    |DEPENDENCY| Add prettier and .prettierrc.yaml

--- src/index.ts
+++ src/index.ts
@@ -7,50 +7,50 @@ console.log("PINGING... ");
 const url = "lol.garena.com";
 
 function colorizeRemote(latency: number | string) {
-     return () => {
-          let color = chalk.red;
-          if (latency < 90) {
-               color = chalk.green;
-          } else if (latency <= 100) {
-               color = chalk.yellow;
-          } else if (latency <= 120) {
-               color = chalk.hex("#ffa500");
-          }
-          if (!(latency >= 0)) {
-               return chalk.white.bgRed(" Timed out ");
-          }
-          return color.bold(` ${latency}`) + color.grey(" ms");
-     };
+	return () => {
+		let color = chalk.red;
+		if (latency < 90) {
+			color = chalk.green;
+		} else if (latency <= 100) {
+			color = chalk.yellow;
+		} else if (latency <= 120) {
+			color = chalk.hex("#ffa500");
+		}
+		if (!(latency >= 0)) {
+			return chalk.white.bgRed(" Timed out ");
+		}
+		return color.bold(` ${latency}`) + color.grey(" ms");
+	};
 }
 function colorizeLocal(latency: number | string) {
-     return () => {
-          let color = chalk.red;
-          if (latency < 1) {
-               color = chalk.green;
-          } else if (latency <= 10) {
-               color = chalk.hex("#ffa500");
-          }
+	return () => {
+		let color = chalk.red;
+		if (latency < 1) {
+			color = chalk.green;
+		} else if (latency <= 10) {
+			color = chalk.hex("#ffa500");
+		}
 
-          return color.bold(latency);
-     };
+		return color.bold(latency);
+	};
 }
 
 let defaultGateway: any;
 network.get_gateway_ip(function (err: any, ip: any) {
-     console.log("Your Router IP", err || ip); // should return your public IP address
+	console.log("Your Router IP", err || ip); // should return your public IP address
 
-     defaultGateway = ip;
+	defaultGateway = ip;
 });
 const pingService = cron.schedule("* * * * * *", async () => {
-     Promise.all([ping.promise.probe(url), ping.promise.probe(defaultGateway)])
-          .then((res) => {
-               console.log(
-                    `Pinging ${url} ${colorizeRemote(
-                         Number(Number(res[0].avg).toFixed(0))
-                    )()}. Gateway (${defaultGateway}) ${colorizeRemote(
-                         Number(Number(res[1].avg).toFixed(0))
-                    )()}  `
-               );
-          })
-          .catch((err) => console.log(err));
+	Promise.all([ping.promise.probe(url), ping.promise.probe(defaultGateway)])
+		.then((res) => {
+			console.log(
+				`Pinging ${url} ${colorizeRemote(
+					Number(Number(res[0].avg).toFixed(0))
+				)()}. Gateway (${defaultGateway}) ${colorizeRemote(
+					Number(Number(res[1].avg).toFixed(0))
+				)()}  `
+			);
+		})
+		.catch((err) => console.log(err));
 });
